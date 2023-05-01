d94d93c7f7c92d7b42a29ab7311d332c4e199e7e
Fernando Caliço <fernando.carvalho.calico@gmail.com>
2023-05-01 13:33:22 -0300

    Alteração na Hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const questions = document.querySelectorAll('[data-faq-question]'); 
 
     for (let i = 0; i < questions.length; i++) {
+        fechaRespostas
         questions[i].addEventListener('click', abreOuFechaResposta);
     } 
 })
+
+function fechaRespostas(elemento) {
+    const classeFechada = 'faqs__questions__items'; 
+    const classeAberta = 'faqs__questions__items--is-open';
+    const elementoPai = elemento.target.parentNode;
+    const classeElementoPai = elementoPai.className
+        
+    if (classeElementoPai == classeAberta) { 
+        elementoPai.classList.toggle(classeFechada);
+    }
+}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faqs__questions__items--is-open';
